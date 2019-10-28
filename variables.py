e7c3b500e43e0ab0d6a07777eb5a9f6534a6d10e
badhwars <43890071+badhwars@users.noreply.github.com>
2019-10-28 09:43:06 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+#declaring a varibale
+
+f=0
+
+print(f)
+
+# changint the variable of variable
+
+f="abc"
+
+print(f)
+
+
+#print("This is a string " + (123)) -> this does not works
+print("This is a string " + str(123))
+
+
+def function():
+    f="def"
+    print(f)
+
