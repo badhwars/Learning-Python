21e533aee0f8083aee9487b46833eca3b2c0d520
badhwars <43890071+badhwars@users.noreply.github.com>
2019-10-28 09:52:59 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,30 @@
+# create new function in python
+
+def function():
+    print("Printing from function")
+
+
+function()
+
+
+# function with 2 arguments
+# adds the arguments to print the result
+
+def sum(arg1 , arg2):
+    print(arg1 + arg2)
+
+
+# function that takes a number 
+# returns the cube of that number
+
+def cube(x):
+    return x*x*x
+
+
+#calling sum function
+
+sum(20,40)
+
+#calling cube function
+
+print(cube(2))
